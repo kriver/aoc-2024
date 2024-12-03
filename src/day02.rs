ec32c47291c4f4594eb7e7413a7621dab3f4c9a7
Kris Verbeeck <kris.verbeeck@worldline.com>
2024-12-03 08:19:36 +0100

    day 02.1

--- src/day02.rs
+++ src/day02.rs
@@ -1,15 +1,41 @@
 use crate::util::load;
 
-pub fn input() -> Vec<String> {
+type Input = Vec<Vec<i32>>;
+
+pub fn input() -> Input {
     let values: Vec<String> = load("data/day02.txt");
     values
+        .into_iter()
+        .map(|s| {
+            s.split_whitespace()
+                .map(|v| v.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect()
 }
 
-pub fn part1(values: Vec<String>) -> u32 {
-    values.len() as u32
+fn is_safe(v: Vec<i32>) -> bool {
+    let incr = v[0] < v[1];
+    for i in 1..v.len() {
+        if incr && v[i - 1] > v[i] {
+            return false;
+        } else if !incr && v[i - 1] < v[i] {
+            return false;
+        }
+        let delta = (v[i - 1] - v[i]).abs();
+        if delta < 1 || delta > 3 {
+            return false;
+        }
+    }
+    return true;
 }
 
-pub fn part2(values: Vec<String>) -> u32 {
+pub fn part1(input: Input) -> usize {
+    // FIXME fix clone()
+    input.into_iter().filter(|v| is_safe(v.clone())).count()
+}
+
+pub fn part2(values: Input) -> u32 {
     values.len() as u32
 }
 
@@ -19,7 +45,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 0);
+        assert_eq!(part1(input()), 670);
     }
 
     #[test]
