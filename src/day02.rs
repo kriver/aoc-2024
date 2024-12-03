d974b737c85d4c703ab91873106a253e221d312a
Kris Verbeeck <kris.verbeeck@worldline.com>
2024-12-03 09:58:31 +0100

    day 02.2

--- src/day02.rs
+++ src/day02.rs
@@ -1,3 +1,5 @@
+use num_traits::signum;
+
 use crate::util::load;
 
 type Input = Vec<Vec<i32>>;
@@ -14,29 +16,48 @@ pub fn input() -> Input {
         .collect()
 }
 
-fn is_safe(v: Vec<i32>) -> bool {
-    let incr = v[0] < v[1];
-    for i in 1..v.len() {
-        if incr && v[i - 1] > v[i] {
-            return false;
-        } else if !incr && v[i - 1] < v[i] {
-            return false;
+fn is_safe(v: &[i32], skip: Option<usize>) -> bool {
+    let mut prev: Option<i32> = None;
+    let mut sigsum = 0;
+    for i in 0..v.len() {
+        if let Some(k) = skip {
+            if i == k {
+                continue;
+            }
         }
-        let delta = (v[i - 1] - v[i]).abs();
-        if delta < 1 || delta > 3 {
-            return false;
+        if let Some(p) = prev {
+            let delta = v[i] - p;
+            if delta.abs() < 1 || delta.abs() > 3 {
+                return false;
+            }
+            sigsum += signum(delta);
+        }
+        prev = Some(v[i]);
+    }
+    sigsum.abs() == v.len() as i32 - (if let None = skip { 1 } else { 2 })
+}
+
+fn is_safe_with_tolerance(v: &[i32]) -> bool {
+    if is_safe(v, None) {
+        return true;
+    }
+    for skip in 0..v.len() {
+        if is_safe(v, Some(skip)) {
+            return true;
         }
     }
-    return true;
+    false
 }
 
 pub fn part1(input: Input) -> usize {
-    // FIXME fix clone()
-    input.into_iter().filter(|v| is_safe(v.clone())).count()
+    input.into_iter().filter(|v| is_safe(v, None)).count()
 }
 
-pub fn part2(values: Input) -> u32 {
-    values.len() as u32
+pub fn part2(input: Input) -> usize {
+    input
+        .into_iter()
+        .filter(|v| is_safe_with_tolerance(v))
+        .count()
 }
 
 #[cfg(test)]
@@ -50,6 +71,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 0);
+        assert_eq!(part2(input()), 700);
     }
 }
