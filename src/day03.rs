aea0722089e2a44398e9d1a82d3d854f9bfc2b8e
Kris Verbeeck <kris.verbeeck@worldline.com>
2024-12-04 07:10:36 +0100

    day 03.2

--- src/day03.rs
+++ src/day03.rs
@@ -9,24 +9,39 @@ pub fn input() -> Input {
     values
 }
 
-pub fn part1(values: Input) -> u32 {
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+fn process(values: Input, do_not: bool) -> u32 {
+    let re = Regex::new(r"(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))").unwrap();
+    let mut enabled = true;
     values
         .into_iter()
         .map(|line| {
             re.captures_iter(&line)
                 .map(|c| {
-                    let a = c.get(1).unwrap().as_str().parse::<u32>().unwrap();
-                    let b = c.get(2).unwrap().as_str().parse::<u32>().unwrap();
-                    a * b
+                    let mut v = 0;
+                    if let Some(_) = c.get(1) {
+                        if enabled {
+                            let a = c.get(2).unwrap().as_str().parse::<u32>().unwrap();
+                            let b = c.get(3).unwrap().as_str().parse::<u32>().unwrap();
+                            v = a * b
+                        }
+                    } else if let Some(_) = c.get(4) {
+                        enabled = true;
+                    } else if let Some(_) = c.get(5) {
+                        enabled = if do_not { false } else { true };
+                    }
+                    v
                 })
                 .sum::<u32>()
         })
         .sum()
 }
 
+pub fn part1(values: Input) -> u32 {
+    process(values, false)
+}
+
 pub fn part2(values: Input) -> u32 {
-    values.len() as u32
+    process(values, true)
 }
 
 #[cfg(test)]
@@ -40,6 +55,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 0);
+        assert_eq!(part2(input()), 53783319);
     }
 }
