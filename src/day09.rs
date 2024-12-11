e711d59d9020aac566d8389819b51da769bcb070
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2024-12-11 15:58:44 +0100

    day 09.1

--- src/day09.rs
+++ src/day09.rs
@@ -1,14 +1,71 @@
-use crate::util::load;
+use std::cmp::min;
 
-type Input = Vec<String>;
+use crate::util::{char2num, load};
+
+type Input = Vec<u8>;
 
 pub fn input() -> Input {
     let values: Vec<String> = load("data/day09.txt");
-    values
+    values[0].chars().map(|c| char2num(c)).collect()
 }
 
-pub fn part1(values: Input) -> u32 {
-    values.len() as u32
+pub fn part1(values: Input) -> usize {
+    fn add_file_blocks(block: &mut usize, file_id: usize, num_blocks: usize) -> usize {
+        let mut checksum = 0;
+        for _ in 0..num_blocks {
+            checksum += *block * file_id;
+            *block += 1;
+        }
+        checksum
+    }
+    let mut checksum = 0;
+    let mut block = 0;
+    let (mut forward, mut backward) = (0, values.len() - 1);
+    let (mut forward_id, mut backward_id) = (0, values.len() / 2);
+    let mut backward_cnt = 0;
+    let mut gap_sz = 0;
+    let mut is_gap = false;
+    loop {
+        let (id, mut cnt, max_sz) = if is_gap {
+            if gap_sz == 0 {
+                gap_sz = values[forward] as usize;
+            }
+            if backward_cnt == 0 {
+                backward_cnt = values[backward] as usize;
+                backward -= 2; // skip free space when moving backwards
+            }
+            (backward_id, backward_cnt, gap_sz)
+        } else {
+            let file_sz = values[forward] as usize;
+            let fill = (forward_id, file_sz, file_sz);
+            forward_id += 1;
+            forward += 1;
+            fill
+        };
+        let sz = min(max_sz, cnt);
+        checksum += add_file_blocks(&mut block, id, sz);
+        if is_gap {
+            backward_cnt -= sz;
+            if backward_cnt == 0 {
+                backward_id -= 1;
+            }
+            gap_sz -= sz;
+            if gap_sz == 0 {
+                forward += 1;
+                is_gap = false;
+            }
+        } else {
+            is_gap = true;
+        }
+        if forward > backward {
+            // fill remainder
+            if backward_cnt > 0 {
+                checksum += add_file_blocks(&mut block, backward_id, backward_cnt);
+            }
+            break;
+        }
+    }
+    checksum
 }
 
 pub fn part2(values: Input) -> u32 {
@@ -21,7 +78,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(input()), 0);
+        assert_eq!(part1(input()), 6344673854800);
     }
 
     #[test]
