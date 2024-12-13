e9bd3b52c27c890cc6cc97965cb6e77197544201
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2024-12-13 15:04:45 +0100

    day 13.2

--- src/day13.rs
+++ src/day13.rs
@@ -2,22 +2,22 @@ use regex::{Captures, Regex};
 
 use crate::util::load;
 
-const PRICE_BUTTON_A: u32 = 3;
-const PRICE_BUTTON_B: u32 = 1;
+const PRICE_BUTTON_A: i64 = 3;
+const PRICE_BUTTON_B: i64 = 1;
 
 #[derive(Debug)]
 pub struct Claw {
-    xa: u32,
-    ya: u32,
-    xb: u32,
-    yb: u32,
-    xp: u32,
-    yp: u32,
+    xa: i64,
+    ya: i64,
+    xb: i64,
+    yb: i64,
+    xp: i64,
+    yp: i64,
 }
 type Input = Vec<Claw>;
 
 pub fn input() -> Input {
-    fn get(c: &Captures, i: usize) -> u32 {
+    fn get(c: &Captures, i: usize) -> i64 {
         c.get(i).unwrap().as_str().parse().unwrap()
     }
     let re = Regex::new(r"^[^\d]*(\d+)[^\d]*(\d+)[^d]*$").unwrap();
@@ -39,22 +39,28 @@ pub fn input() -> Input {
     claws
 }
 
-pub fn part1(claws: Input) -> u32 {
-    fn cost(c: Claw) -> u32 {
-        for a in 1..=100 {
-            for b in 1..=100 {
-                if (a * c.xa + b * c.xb == c.xp) && (a * c.ya + b * c.yb == c.yp) {
-                    return a * PRICE_BUTTON_A + b * PRICE_BUTTON_B;
-                }
-            }
+fn cost(c: Claw, offset: i64) -> i64 {
+    let mut cost = 0;
+    let b_num = (c.yp + offset) * c.xa - (c.xp + offset) * c.ya;
+    let b_den = c.xa * c.yb - c.xb * c.ya;
+    if b_num % b_den == 0 {
+        let b = b_num / b_den;
+        let a_num = (c.xp + offset) - c.xb * b;
+        let a_den = c.xa;
+        if a_num % a_den == 0 {
+            let a = a_num / a_den;
+            cost = a * PRICE_BUTTON_A + b * PRICE_BUTTON_B;
         }
-        0
     }
-    claws.into_iter().map(|c| cost(c)).sum()
+    cost
 }
 
-pub fn part2(values: Input) -> u32 {
-    values.len() as u32
+pub fn part1(claws: Input) -> i64 {
+    claws.into_iter().map(|c| cost(c, 0)).sum()
+}
+
+pub fn part2(claws: Input) -> i64 {
+    claws.into_iter().map(|c| cost(c, 10000000000000)).sum()
 }
 
 #[cfg(test)]
@@ -68,6 +74,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 0);
+        assert_eq!(part2(input()), 96979582619758);
     }
 }
