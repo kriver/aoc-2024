b4dab873ba30e0d38319578b56c8e4a12e572489
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2024-12-25 10:03:54 +0100

    day 23.2

--- src/day23.rs
+++ src/day23.rs
@@ -3,7 +3,7 @@ use std::collections::{HashMap, HashSet};
 
 use crate::util::load;
 
-type Input = HashMap<String, Vec<String>>;
+type Input = HashMap<String, HashSet<String>>;
 
 pub fn input() -> Input {
     let lines: Vec<String> = load("data/day23.txt");
@@ -12,8 +12,10 @@ pub fn input() -> Input {
         let mut token_it = line.split("-");
         let a = token_it.next().unwrap().to_string();
         let b = token_it.next().unwrap().to_string();
-        con.entry(a.clone()).or_insert(Vec::new()).push(b.clone());
-        con.entry(b).or_insert(Vec::new()).push(a);
+        con.entry(a.clone())
+            .or_insert(HashSet::new())
+            .insert(b.clone());
+        con.entry(b).or_insert(HashSet::new()).insert(a);
     }
     con
 }
@@ -32,8 +34,51 @@ pub fn part1(input: Input) -> usize {
     triangles.len()
 }
 
+/// https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm#Without_pivoting
+// algorithm BronKerbosch1(R, P, X) is
+//     if P and X are both empty then
+//         report R as a maximal clique
+//     for each vertex v in P do
+//         BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+//         P := P \ {v}
+//         X := X ⋃ {v}
 pub fn part2(input: Input) -> String {
-    "".to_string()
+    fn bron_kerbosch(
+        n: &Input, // neighbours
+        r: HashSet<String>,
+        mut p: HashSet<String>,
+        mut x: HashSet<String>,
+    ) -> Option<HashSet<String>> {
+        if p.is_empty() && p.is_empty() {
+            return Some(r);
+        }
+        let mut clique = HashSet::new();
+        while !p.is_empty() {
+            let v = p.iter().next().unwrap().to_string();
+            if let Some(c) = bron_kerbosch(
+                n,
+                r.union(&HashSet::from([v.clone()])).cloned().collect(),
+                p.intersection(n.get(&v).unwrap()).cloned().collect(),
+                x.intersection(n.get(&v).unwrap()).cloned().collect(),
+            ) {
+                if c.len() > clique.len() {
+                    clique = c
+                }
+            }
+            p.remove(&v);
+            x.insert(v.to_string());
+        }
+        Some(clique)
+    }
+    let r = HashSet::new();
+    let p = input.keys().cloned().collect();
+    let x = HashSet::new();
+    let mut c = bron_kerbosch(&input, r, p, x)
+        .unwrap()
+        .into_iter()
+        .collect::<Vec<String>>();
+    c.sort();
+    c.join(",")
 }
 
 #[cfg(test)]
@@ -47,6 +92,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), "");
+        assert_eq!(part2(input()), "ca,dw,fo,if,ji,kg,ks,oe,ov,sb,ud,vr,xr");
     }
 }
