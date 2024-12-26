425c3f9d3f7a07d6b123b8caaa646502fb94cb5e
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2024-12-26 11:13:03 +0100

    day 06.2

--- src/day06.rs
+++ src/day06.rs
@@ -2,24 +2,74 @@ use std::collections::{HashMap, HashSet};
 
 use crate::util::{load, Coord2D, Direction};
 
-type Obstacles = HashSet<Coord2D<i32>>;
-type Path = HashMap<Coord2D<i32>, Vec<Direction>>;
+type Coord = Coord2D<i32>;
+type Obstacles = HashSet<Coord>;
+type Path = HashMap<Coord, HashSet<Direction>>;
 
 pub struct Input {
-    guard: Coord2D<i32>,
+    guard: Coord,
     dir: Direction,
     obstacles: Obstacles,
-    sz: Coord2D<i32>,
+    sz: Coord,
 }
 
-fn move_dir(mut c: Coord2D<i32>, dir: &Direction) -> Coord2D<i32> {
-    match dir {
-        Direction::Up => c.y -= 1,
-        Direction::Down => c.y += 1,
-        Direction::Left => c.x -= 1,
-        Direction::Right => c.x += 1,
+impl Input {
+    fn is_off_map(&self, Coord { x, y }: Coord) -> bool {
+        x < 0 || y < 0 || x >= self.sz.x || y >= self.sz.y
     }
-    c
+
+    /// returns true if patrol ends up in an endless loop
+    fn try_patrol(
+        &mut self,
+        mut pos: Coord,
+        mut dir: Direction,
+        path: &mut Path,
+        mut loop_obstacles: Option<&mut Obstacles>,
+    ) -> bool {
+        let mut lp: Path = Path::new();
+        loop {
+            let new_pos = pos + dir.into();
+            if self.is_off_map(new_pos) {
+                break false;
+            }
+            if self.obstacles.contains(&new_pos) {
+                dir = dir.turn_right();
+            } else {
+                match loop_obstacles {
+                    Some(ref mut lo) => {
+                        // check whether an obstacle would result in a loop
+                        if !path.contains_key(&new_pos) {
+                            // only if path hasn't crossed here before!!!
+                            self.obstacles.insert(new_pos);
+                            if self.try_patrol(pos, dir, path, None) {
+                                lo.insert(new_pos);
+                            }
+                            self.obstacles.remove(&new_pos);
+                        }
+                        path.entry(new_pos).or_insert(HashSet::new()).insert(dir);
+                    }
+                    None => {
+                        let in_path = path.contains_key(&new_pos) && path[&new_pos].contains(&dir);
+                        let in_lp = lp.contains_key(&new_pos) && lp[&new_pos].contains(&dir);
+                        if in_path || in_lp {
+                            break true;
+                        } else {
+                            lp.entry(new_pos).or_insert(HashSet::new()).insert(dir);
+                        }
+                    }
+                }
+                pos = new_pos;
+            }
+        }
+    }
+
+    fn patrol(&mut self) -> (Path, Obstacles) {
+        let mut loop_obstacles = HashSet::new();
+        let mut path = Path::new();
+        path.insert(self.guard, HashSet::from([self.dir]));
+        self.try_patrol(self.guard, self.dir, &mut path, Some(&mut loop_obstacles));
+        (path, loop_obstacles)
+    }
 }
 
 pub fn input() -> Input {
@@ -53,43 +103,15 @@ pub fn input() -> Input {
     }
 }
 
-fn patrol(input: &mut Input) -> Path {
-    let mut pos = input.guard;
-    let mut dir = input.dir;
-    let mut path = HashMap::new();
-    path.insert(pos, vec![dir]);
-    loop {
-        let new_pos = move_dir(pos, &dir);
-        let Coord2D { x, y } = new_pos;
-        if x < 0 || y < 0 || x >= input.sz.x || y >= input.sz.y {
-            break;
-        }
-        if input.obstacles.contains(&new_pos) {
-            dir = match dir {
-                Direction::Up => Direction::Right,
-                Direction::Down => Direction::Left,
-                Direction::Left => Direction::Up,
-                Direction::Right => Direction::Down,
-            }
-        } else {
-            path.entry(new_pos)
-                .and_modify(|v| v.push(dir))
-                .or_insert(vec![dir]);
-            pos.x = x;
-            pos.y = y;
-        }
-    }
-    path
-}
-
 pub fn part1(mut input: Input) -> usize {
-    let path = patrol(&mut input);
-    path.len()
+    let (path, _) = input.patrol();
+    let distinct: HashSet<_> = path.into_iter().map(|(c, _)| c).collect();
+    distinct.len()
 }
 
-pub fn part2(mut input: Input) -> u32 {
-    let _path = patrol(&mut input);
-    0
+pub fn part2(mut input: Input) -> usize {
+    let (_, loop_obstacles) = input.patrol();
+    loop_obstacles.len()
 }
 
 #[cfg(test)]
@@ -103,6 +125,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 0);
+        assert_eq!(part2(input()), 1972);
     }
 }
